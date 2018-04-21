a21c610ba09db2229eee0da6e8d2d080938e805f
wiktortr <wiktortr9@gmail.com>
2018-04-21 17:26:35 +0200

    Add Communication Between Windows

--- app.js
+++ app.js
@@ -8,25 +8,61 @@ const path = require("path");
 const url = require("url");
 const fs = require("fs");
 
+let splashScreen;
+let mainwindow;
+let loaded = false;
+
+function close_windows(){
+    mainwindow = null;
+    splashScreen = null;
+    app.exit();
+}
+
 app.on("ready", function(){
 
-    let mainwindow = new electron.BrowserWindow({
+    splashScreen = new electron.BrowserWindow({
         width: 400,
         height: 490,
-        icon: path.join(__dirname, '/iconT.ico'),
+        icon: path.join(__dirname, "ikonaT.ico"),
         frame: false
+    });
 
+    splashScreen.loadURL(url.format({
+        pathname: path.join(__dirname, "splashScreen.html"),
+        protocol: "file:",
+        slashes: true
+    }));
+
+    let mainwindowSize = electron.screen.getPrimaryDisplay().workAreaSize
+
+    mainwindow = new electron.BrowserWindow({
+        width: mainwindowSize.width,
+        height: mainwindowSize.height,
+        icon: path.join(__dirname, "ikonaT.ico"),
+        show: false
     });
 
     mainwindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file:',
+        pathname: path.join(__dirname, "index.html"),
+        protocol: "file:",
         slashes: true
     }));
-    
-    mainwindow.on('closed', function(){
-        mainwindow = null;
-        app.exit();
+
+    splashScreen.on("closed", function(){
+        if(loaded){
+            splashScreen = null;
+        }
+        else{
+            close_windows();
+        }
+    });
+    mainwindow.on("closed", close_windows); 
+
+    ipc.on("load_msg", function(event, data){
+        splashScreen.webContents.send("splash-screen-msg", data);
+        if(data.state == 10){
+            loaded = true;
+        }
     });
 
 });
--- renderer.js
+++ renderer.js
@@ -1,4 +1,49 @@
+const electron = require("electron");
+const remote = electron.remote;
+const ipc = electron.ipcRenderer;
+const dialog = remote.dialog;
+const Menu = remote.Menu;
+const Tray = remote.Tray;
+const Window = remote.getCurrentWindow();
+const WebContext = remote.getCurrentWebContents();
+
+const fs = require("fs");
+const path = require("path");
+
+const res_to_load = [
+    "modules"
+];
+
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds){
+        break;
+      }
+    }
+  }
+
+function loadRes(p){
+    console.log(p);
+    return {
+        state: 0,
+        res: path.basename(p)
+    }
+}
 
 $(document).ready(function(){
+
+    for (let i = 0; i < res_to_load.length; i++) {
+        let list = fs.readdirSync(path.join(__dirname, res_to_load[i]));
+        for (let j = 0; j < list.length; j++) {
+            //setTimeout(function(){
+                ipc.send("load_msg", loadRes(list[j]));
+                sleep(1000);
+            //},1000);
+        }
+    }
+    ipc.send("load_msg", {state: 10});
+    Window.show();
+    Window.maximize();
 
 });
